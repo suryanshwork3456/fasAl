4aee3ccdd8077de2621b5bc2d00fef0198972cac
suryanshwork3456 <suryanshwork3456@gmail.com>
2026-08-29 19:52:53 +0530

    Ran alembic migrations

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -4,6 +4,7 @@ from app.models.field import Field
 from app.models.dashboard import Dashboard
 from app.models.field_form import FieldForm
 from app.models.crop_health import StressZone, CropHealthMetric
+from app.models.crop import CropAnalysis  # <--- Add this import
 
 __all__ = [
     "User",
@@ -12,4 +13,5 @@ __all__ = [
     "FieldForm",
     "StressZone",
     "CropHealthMetric",
+    "CropAnalysis",  # <--- Add this to __all__
 ]
