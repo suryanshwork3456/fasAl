82f50abeb1250c7e4e17aac1d601acf20fc0a37a
Gaurav Manoj Singh <singhgauravvv558@gmail.com>
2026-08-29 10:15:00 +0530

    Delete auth directory

--- auth/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-class PhoneRequest(BaseModel):
-    phone: str = Field(..., example="1234567890")
-
-class VerifyOTPRequest(BaseModel):
-    phone: str = Field(..., example="1234567890")
-    otp: str = Field(..., example="1234")
