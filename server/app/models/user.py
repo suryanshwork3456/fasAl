e06850204c7b3da5e464d6eef8ef4396fccdee26
suryanshwork3456 <suryanshwork3456@gmail.com>
2026-08-29 15:30:51 +0530

    Added models

---
+++ server/app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.db.session import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    
+    fields: Mapped[list["Field"]] = relationship("Field", back_populates="owner", cascade="all, delete-orphan")
