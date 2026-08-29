e3a53d795f44e02615b81b35c06ab3f49ae94950
Gaurav Manoj Singh <singhgauravvv558@gmail.com>
2026-08-29 10:26:31 +0530

    Add authentication router and Redis support

--- server/app/main.py
+++ server/app/main.py
@@ -2,11 +2,12 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import api_v1_router
-
+from auth import auth_router
+from redis import redis
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
-
+    redis.close()
 app = FastAPI(title="fasAI", lifespan=lifespan)
 
 # Add CORS Middleware (Note: origins must be strings)
@@ -20,6 +21,7 @@ app.add_middleware(
 
 # Include Router
 app.include_router(api_v1_router, prefix="/api/v1")
+app.include_router(auth_router,prefix="/api/v1/auth",tags=["Auth"])
 
 @app.get("/")
 def root():
